32daed081ab63f0f499916d6ae51d77efac3d18d
Ivan <vanya-egorov@mail.ru>
2021-08-30 13:24:23 +0300

    alfa

--- lesson09/FileManager/FileManager/Program.cs
+++ lesson09/FileManager/FileManager/Program.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows;
 
 namespace FileManager
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            
+            while (true)
+            {
 
-            Console.ReadKey();
+                Comands.Get_Command();
+                
+                
+                //Comands.Window();
 
+
+
+                //Console.SetCursorPosition(0, 22);
+                //Console.WriteLine("Введите путь к директории:");
+                //Console.WriteLine("Для выхода нажмите крестик");
+                //Console.SetCursorPosition(0, 27);
+                //Tree.GetDir();
+              
+                
+            }
         }
     }
 }
---
+++ lesson09/FileManager/FileManager/Walls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+
+            //Отрисовка линий
+
+            HorizontalLine upLine1 = new HorizontalLine(0, mapWidth - 2, 26, '=');
+            HorizontalLine upLine2 = new HorizontalLine(0, mapWidth - 2, 20, '=');
+
+            wallList.Add(upLine1);
+            wallList.Add(upLine2);
+        }
+        public void Draw()
+        {
+            foreach (var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
